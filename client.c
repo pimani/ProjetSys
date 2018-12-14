e43b0741f9c9293976854a0d3d7d16581bdad3d0
Pimani <pimanipb@gmail.com>
2018-12-14 23:00:40 +0100

    Ouverture des tubes plus tard

--- client.c
+++ client.c
@@ -15,17 +15,19 @@
 #include "client.h"
 
 int main(void) {
+  printf("Démarage du client : \n");
   char wtubename[MAX_NOM_SIZE];
   strcpy(wtubename, NOM_FIFOW);
   sprintf(wtubename + strlen(wtubename), "%d", getpid());
   char rtubename[MAX_NOM_SIZE];
   strcpy(rtubename, NOM_FIFOR);
   sprintf(rtubename + strlen(rtubename), "%d", getpid());
-
+  printf("Creation des tubes :\nCreation tube lecture\n");
   if (mkfifo(wtubename, S_IRUSR | S_IWUSR) == -1) {
     perror("mkfifo wtube");
     exit(EXIT_FAILURE);
   }
+  printf("Creation tube écriture\n");
   if (mkfifo(rtubename, S_IRUSR | S_IWUSR) == -1) {
     perror("mkfifo rtube");
     exit(EXIT_FAILURE);
@@ -36,22 +38,23 @@ int main(void) {
     perror("open wtube");
     exit(EXIT_FAILURE);
   }
-  dup2(0, fdw);
 
   int fdr = open(rtubename, O_RDONLY);
   if (fdr == -1) {
     perror("open rtube");
     exit(EXIT_FAILURE);
   }
-  dup2(1, fdr);
 
   argsc cmd;
+  printf("Donner vos argument, le premier étant la commande :\n");
   for (int i = 0; i < 3; ++i) {
+    printf("Argument numéro %d : ", i);
     if (fgets(cmd.argv[i], sizeof(cmd.argv[i]), stdin) == NULL) {
       perror("fgets");
       exit(EXIT_FAILURE);
     }
   }
+  printf("Donner vos variables d'environements:");
   if (fgets(cmd.envp, sizeof(cmd.envp), stdin) == NULL) {
     perror("fgets");
     exit(EXIT_FAILURE);
@@ -65,6 +68,9 @@ int main(void) {
     perror("ouverture file");
     exit(EXIT_FAILURE);
   }
+  printf("Envois du méssage et remplacement des entrée et sortie standards\n");
+  dup2(0, fdw);
+  dup2(1, fdr);
   file_ajout((info *) descriptor, &cmd);
 
   return EXIT_SUCCESS;
